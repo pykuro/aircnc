2d4e13b1de46fbca924edaa31d1e29c989e8151a
Paula Kuromoto <paula.kuromoto@gmail.com>
2019-10-25 13:08:25 -0300

    mobile

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,8 @@ const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 const DashboardController = require('./controllers/DashboardController');
 const BookingController = require('./controllers/BookingController');
+const ApprovalController = require('./controllers/ApprovalController');
+const RejectionController = require('./controllers/RejectionController');
 
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
@@ -17,5 +19,8 @@ routes.get('/spots', SpotController.index);
 routes.get('/dashboard', DashboardController.show);
 
 routes.post('/spots/:spot_id/bookings', BookingController.store);
+
+routes.post('/bookings/:booking_id/approvals', ApprovalController.store);
+routes.post('/bookings/:booking_id/rejections', RejectionController.store);
 
 module.exports = routes;
